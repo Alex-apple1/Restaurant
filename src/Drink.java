ee6f434673d73d2e64aea91edc78a7ec500b0184
Alex <alex-apple1@yandex.ru>
2021-04-24 15:02:50 +0300

    added text to branch2

--- src/Drink.java
+++ src/Drink.java
@@ -3,4 +3,6 @@ public class Drink extends BaseDish {
     Drink(String name, double price) {
         super(name, price);
     }
+
+    //Some shit to be added here
 }
--- src/Side.java
+++ src/Side.java
@@ -3,4 +3,6 @@ public class Side extends BaseDish{
     Side(String name, double price) {
         super(name, price);
     }
+
+    //Some shit to be added here
 }
